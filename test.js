e218186180fbe5ee4b5d96795c23af1fe56f8b43
Ray <raymond@thelyonfamily.net>
2022-04-25 18:52:05 -0500

    Light and dark theme

--- test.js
+++ test.js
@@ -1,5 +1,20 @@
+console.log("Connected to js file");
 
-for (let i = 0; i < 32; i++){
-  console.log(i);
-}
-console.log("Javascript is just as ass as java");
+let numClicks = 0;
+const button = document.createElement('button');
+button.innerHTML = "Light";
+
+document.body.appendChild(button);
+button.addEventListener('click', () =>{
+  numClicks ++;
+  if (numClicks %2 == 0){
+    button.innerHTML = "Light";
+    theme.setAttribute('href', './css/light.css');
+    //alert('Number of clicks: ' + numClicks);
+  }
+  else if(numClicks %2 ==1){
+    button.innerHTML = "Dark";
+    console.log("odd number");
+    theme.setAttribute('href', './css/dark.css');
+  }
+})
